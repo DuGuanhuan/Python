825febd56180fbb9d5018a0a643bfc4f87b15952
DuGuanhuan <2536857143@qq.com>
2023-12-13 18:36:45 +0800

    Update quiz1.py

--- Lab12/quiz1.py
+++ Lab12/quiz1.py
@@ -1,18 +1,30 @@
 import csv
-file_name = input("Enter subject file name: ")
 
-column_len1 = int(input("Enter 1st column length: "))
-column_len2 = int(input("Enter 2nd column length: "))
-column_len3 = int(input("Enter 3rd column length: "))
+def read_and_display_file():
+    # Get user input
+    file_name = input("Enter subject file name: ")
+    col1_width = int(input("Enter 1st column length: "))
+    col2_width = int(input("Enter 2nd column length: "))
+    col3_width = int(input("Enter 3rd column length: "))
 
-with open(file_name, 'r') as file:
-    reader = csv.DictReader(file)
+    try:
+        # Open and read the CSV file
+        with open(file_name, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            
+            # Print the header with specified column widths and alignment
+            header_format = "{:<{col1_width}}{:^{col2_width}}{:>{col3_width}}"
+            row_format = "{:<{col1_width}}{:^{col2_width}}{:>{col3_width}}"
+            print(header_format.format("Code", "Name", "CP", col1_width=col1_width, col2_width=col2_width, col3_width=col3_width))
+            
+            # Print each row with specified column widths and alignment
+            for row in reader:
+                print(row_format.format(row['code'], row['name'], row['cp'], col1_width=col1_width, col2_width=col2_width, col3_width=col3_width))
 
-    print('Code'.ljust(column_len1) + 'Name'.center(column_len2) + 'CP'.rjust(column_len3))
+    except FileNotFoundError:
+        print("File not found. Please check the file name and try again.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-        # Print subject information
-    for row in reader:
-        subject_code = row.get("code")
-        subject_name = row.get("name")
-        cp = row.get("cp")
-        print(subject_code.ljust(column_len1) + subject_name.center(column_len2) + cp.rjust(column_len3))
+# Run the function
+read_and_display_file()
